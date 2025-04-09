108c9faaff20a3134628d046982a0748ab129713
Tribhuvan nath <tribhuvannath4567@gmail.com>
2025-04-09 13:22:49 +0530

    Togetha - The ultimate social media app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./pages/Authentication/Login";
+import Register from "./pages/Authentication/Register";
+import Authentication from "./pages/Authentication/Authentication";
+
+const App = () => {
+  return (
+    <>
+      <Authentication />
+    </>
+  );
+};
+
+export default App;
